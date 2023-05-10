3e7efc7840773de82c1a6894512960e9e629e9fa
leeroyjenkins123 <kapustin211timof@gmail.com>
2023-05-10 21:01:06 +0700

    Add .h file for interface

---
+++ src/Static_Libs/interface.h
@@ -0,0 +1,10 @@
+#pragma once
+
+int data_check(char word[], int range_min, int range_max);
+void option_select(
+        int* pass_length,
+        int* pass_amount,
+        int* add_numbers,
+        int* add_lowercase_letters,
+        int* add_capital_letters,
+        int* add_special_characters);
