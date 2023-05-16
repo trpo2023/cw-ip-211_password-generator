3d5f18269df4d30227753796907a468b206e2ddf
Rouva Huu <ulyana.bahc2015@yandex.ru>
2023-05-16 23:27:10 +0700

    Interface update

--- Password_Generator/password_generator.h
+++ Password_Generator/password_generator.h
@@ -4,34 +4,42 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class Password_Generator;
-}
+namespace Ui { class Password_Generator; }
 QT_END_NAMESPACE
 
-class Password_Generator : public QMainWindow {
+class Password_Generator : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    Password_Generator(QWidget* parent = nullptr);
+    Password_Generator(QWidget *parent = nullptr);
     ~Password_Generator();
 
 private slots:
-    void on_number_of_pass_activated(int index);
+    void print_password(QString* password, int* counting);
 
-    void on_length_of_pass_activated(int index);
+    void generate_symbol(QString* password, int* i, const char* str);
 
-    void on_numbers_stateChanged(int arg1);
+    void generate_password(QString* password, int* length, int* counting, int character_variants_array[], int* character_variants_count);
 
-    void on_lowercase_letters_stateChanged(int arg1);
+    void fill_options_arr(int* add_numbers, int* add_lowercase_letters, int* add_capital_letters,
+                 int* add_special_characters, int character_variants_array[]);
 
-    void on_capital_letters_stateChanged(int arg1);
+    int on_number_of_pass_activated();
 
-    void on_special_characters_stateChanged(int arg1);
+    int on_length_of_pass_activated();
+
+    int on_numbers_stateChanged();
+
+    int on_lowercase_letters_stateChanged();
+
+    int on_capital_letters_stateChanged();
+
+    int  on_special_characters_stateChanged();
 
     void on_pushButton_clicked();
 
 private:
-    Ui::Password_Generator* ui;
+    Ui::Password_Generator *ui;
 };
 #endif // PASSWORD_GENERATOR_H
