76c19f20d0711f6bb8d05f973d93cdbe6abde76b
leeroyjenkins123 <kapustin211timof@gmail.com>
2023-05-19 00:21:03 +0700

    Include header

--- src/Static_Libs/input_func.cpp
+++ src/Static_Libs/input_func.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "input_func.h"
+
 // checking the entered data
 int data_check(char word[], int range_min, int range_max)
 {
@@ -12,14 +14,14 @@ int data_check(char word[], int range_min, int range_max)
         return 1;
     }
     // if word is not empty and not a positive number
-    for (int i = 0; i < int(strlen(word)); i++) {
+    for (int i = 0; i < (int)(strlen(word)); i++) {
         if (isdigit(word[i]) == 0) {
             // error
             return 1;
         }
     }
     // if word is digit and is in range
-    if (isdigit(*word) == 1 && int(atol(word)) >= range_min
+    if (isdigit(*word) == 1 && (int)(atol(word)) >= range_min
         && int(atol(word)) <= range_max) {
         // correct data entered
         return 0;
